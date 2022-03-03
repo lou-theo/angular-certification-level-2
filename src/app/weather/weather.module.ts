4b22ef0fd2953d7ea1a35643f76cdf6cb638c1f3
Lou-Theo <laurent.lt@sfeir.com>
2022-03-03 14:56:24 +0100

    put back templates for each component

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -4,9 +4,10 @@ import { CurrentCityListComponent } from './current-city-list/current-city-list.
 import { ForecastCityComponent } from './forecast-city/forecast-city.component';
 import { CurrentCityComponent } from './current-city/current-city.component';
 import { WeatherRoutingModule } from './weather-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    imports: [CommonModule, WeatherRoutingModule],
+    imports: [CommonModule, WeatherRoutingModule, SharedModule],
     declarations: [CurrentCityListComponent, ForecastCityComponent, CurrentCityComponent],
 })
 export class WeatherModule {}
