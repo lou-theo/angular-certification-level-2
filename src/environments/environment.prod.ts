f01b955b82b1041f9df9c7c5876c896ebbe7523c
Lou-Theo <laurent.lt@sfeir.com>
2022-03-05 19:47:15 +0100

    add production build settings

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+import { EnvironmentModel } from '@app/core/models/environment.model';
+
+export const environment: EnvironmentModel = {
+    production: true,
+    openweathermapApi: {
+        appId: '5a4b2d457ecbef9eb2a71e480b947604',
+        rootUrl: 'https://api.openweathermap.org/data/2.5/',
+    },
+    iconsRootUrl: 'https://www.angulartraining.com/images/weather/',
+    numberOfForecastedDays: 5,
+    currentWeatherCacheInMinutes: 5,
+};
