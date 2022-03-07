5a7884285ee2768331c381aad021023c1e323682
Lou-Theo <laurent.lt@sfeir.com>
2022-03-07 09:07:50 +0100

    remove unused test files

--- src/app/weather/weather-icon-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WeatherIconUrlPipe } from './weather-icon-url.pipe';
-
-describe('WeatherIconUrlPipe', () => {
-    it('create an instance', () => {
-        const pipe = new WeatherIconUrlPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
